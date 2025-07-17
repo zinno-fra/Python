f5be1401795088857e48f8e3b0f204849c5bb03c
zinno-fra <cvepfd@gmail.com>
2025-07-17 12:58:43 -0400

    Update filescannerv2.py

--- filescannerv2.py
+++ filescannerv2.py
@@ -10,6 +10,6 @@ target_extension = ".ova"#set extension looking for
 
 #list all files in the folder
 for file_name in os.listdir(folder):#starts loop that goes through each file and folder name inside folder specified | os.listdir(folder) gives a list of names of everything in 
-                                    #that folder, files and subfolders. for file_name in means take each name one at a time and call it file_name, to then do something with filename inside the loop
+                                    #that folder, files and subfolders. for file_name in means take each name one at a time and call it file_name, to then do something with file_name inside the loop
     if file_name.lower().endswith(target_extension):#file_name.lower makes the name lowercase, .endswith checks the name endswith desired extension
         print(file_name)#name to print if matches desired extension
